b583e04591a7c57b7f78367bcfa4496b069ddafe
Hardhik Mohanty <66200271+hardhik-99@users.noreply.github.com>
2022-11-01 14:40:39 +0530

    Update mnist_cnn_tpu.py

--- mnist_cnn_tpu.py
+++ mnist_cnn_tpu.py
@@ -31,16 +31,18 @@ def load_tflite_model(modelpath):
 
 interpreter = load_tflite_model(mnist_tflite_filename)
 
-def tflite_predict(interpreter, data):
+def tpu_tflite_predict(interpreter, data):
     input_data = data.reshape(1, 28, 28, 1).astype(np.float32)
     interpreter.set_tensor(interpreter.get_input_details()[0]['index'], input_data)
     interpreter.invoke()
     return interpreter.get_tensor(interpreter.get_output_details()[0]['index'])
 
-#pred = tflite_predict(interpreter, x_test[9])
-#print(pred.argmax(1), y_test[9])
-print(x_test.shape)
+y_pred = []
 
 for i in range(x_test.shape[0]):
-    pred = tflite_predict(interpreter, x_test[i])
-    print(pred.argmax(1), y_test[i])
+    pred = tpu_tflite_predict(interpreter, x_test[i])
+    y_pred = pred.argmax(1)[0]
+    print("Pred: ", pred.argmax(1), " True: ", y_test[i])
+    
+print("Accuracy: ", np.sum(y_pred == y_test) / len(y_pred))
+    
