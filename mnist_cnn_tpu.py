5d152399ab9f6ff1fbacf8a932ced152ac4a7325
Hardhik Mohanty <66200271+hardhik-99@users.noreply.github.com>
2022-11-01 12:25:44 +0530

    Update mnist_cnn_tpu.py

--- mnist_cnn_tpu.py
+++ mnist_cnn_tpu.py
@@ -4,7 +4,17 @@ Created on Thu Oct 20 15:09:14 2022
 
 @author: hardh
 """
+import tensorflow as tf
 import tflite_runtime.interpreter as tflite
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train, x_test = x_train / 255.0, x_test / 255.0
+x_train = x_train.reshape(-1, 28, 28, 1)
+x_test = x_test.reshape(-1, 28, 28, 1)
+
 mnist_tflite_filename = "mnist.tflite"
 
 def load_tflite_model(modelpath):
